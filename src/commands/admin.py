b79b1c0cf50e063347ab33230f912fc39c1575a1
Ananazo <48922919+Ananazo@users.noreply.github.com>
2023-10-03 20:24:23 +0300

    database works now

--- src/commands/admin.py
+++ src/commands/admin.py
@@ -8,25 +8,25 @@ import os
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        mydb = mysql.connector.connect(
-        host=os.getenv('HOST'),
-        user=os.getenv('USER'),
-        password=os.getenv('PASS'),
-        database="ModBot"
+        self.mydb = mysql.connector.connect(
+            host=os.getenv('HOST'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASS'),
+            database="ModBot"
         )
 
-        mycursor = mydb.cursor()
+        self.mycursor = self.mydb.cursor()
 
     @commands.slash_command(description="Clear messages")
     @commands.has_permissions(administrator=True)
-    async def clear(self, ctx: commands.Context, amount: Option(int, "How many messages to delete (default 5)", required = False, default = 5)):
+    async def clear(self, ctx: commands.Context, amount: Option(int, "How many messages to delete (default 5)", required=False, default=5)):
+        amount += 1
         await ctx.defer()
         await ctx.channel.purge(limit=amount)
-        sql = "INSERT INTO clemess (Date, Channel, Deleter, Count, Content) VALUES (%s, %s, %s, %s)"
-        val = (datetime.datetime.now(), ctx.channel, ctx.author, str(amount))
+        sql = "INSERT INTO clemess (Date, Channel, Deleter, Count) VALUES (%s, %s, %s, %s)"
+        val = (datetime.datetime.now(), ctx.channel.id, ctx.author.id, str(amount))
         self.mycursor.execute(sql, val)
         self.mydb.commit()
-        print(self.mycursor.rowcount, "record inserted.")
 
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
