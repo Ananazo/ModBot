bdc2507ea7ccac858679da53ed34422a0de86b82
Ananazo <48922919+Ananazo@users.noreply.github.com>
2023-10-03 20:01:20 +0300

    trying to fix database issues

--- src/commands/admin.py
+++ src/commands/admin.py
@@ -5,30 +5,28 @@ from discord.commands import Option
 import mysql.connector
 import os
 
-mydb = mysql.connector.connect(
-  host=os.getenv('HOST'),
-  user=os.getenv('USER'),
-  password=os.getenv('PASS'),
-  database="ModBot"
-)
-
-mycursor = mydb.cursor()
-
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        mydb = mysql.connector.connect(
+        host=os.getenv('HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASS'),
+        database="ModBot"
+        )
+
+        mycursor = mydb.cursor()
 
     @commands.slash_command(description="Clear messages")
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx: commands.Context, amount: Option(int, "How many messages to delete (default 5)", required = False, default = 5)):
         await ctx.defer()
-        delete = ctx.channel.purge(limit=amount)
-        await delete
-        sql = "INSERT INTO `cleared messages` (Date, Channel, Deleter, Count, Content) VALUES (%s, %s, %s, %s, %s)"
-        val = (datetime.datetime.now(), ctx.channel, ctx.author, amount, delete)
-        mycursor.execute(sql, val)
-        mydb.commit()
-        print(mycursor.rowcount, "record inserted.")
+        await ctx.channel.purge(limit=amount)
+        sql = "INSERT INTO `cleared messages` (Date, Channel, Deleter, Count, Content) VALUES (%s, %s, %s, %s)"
+        val = (datetime.datetime.now(), ctx.channel, ctx.author, str(amount))
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
+        print(self.mycursor.rowcount, "record inserted.")
 
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
